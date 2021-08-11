8bd1b1fe177b49ec6738dafbddfe7bb1635eb74d
yatharthk2 <yatharth.kapadia.1702@gmail.com>
2021-08-12 05:29:34 +0530

    organised the repo

--- image_utils/data_augmentation/data_aug.py
+++ image_utils/data_augmentation/data_aug.py
@@ -8,9 +8,9 @@ from torch.utils.data import DataLoader
 trans = transforms.Compose([
     transforms.ToPILImage(),
     transforms.Resize((256,256)),
-    transforms.RandomCrop((224,224)),
-    transforms.ColorJitter(brightness = 0.5),
-    transforms.RandomRotation(degrees = 45) , 
+    #transforms.RandomCrop((224,224)),
+    #transforms.ColorJitter(brightness = 0.3),
+    #transforms.RandomRotation(degrees = 45) , 
     transforms.RandomVerticalFlip(p=0.5),
     transforms.RandomGrayscale(p=0.1),
     transforms.RandomHorizontalFlip(p = 0.5),
@@ -19,7 +19,7 @@ trans = transforms.Compose([
 ])
 
 
-dataset = DataLoader(ImageDataset(root_dir='data' , transform=trans))
+dataset = DataLoader(ImageDataset(root_dir='D:\\work\\projects\\3D-Crater\\Dataset\\Total' , transform=trans))
 count = 0
 '''for img in dataset :
     img_num = 0
@@ -32,6 +32,6 @@ img_num = 0
 while img_num < 5 :
     count = 0
     for img in dataset:
-        save_image( img,'resized_img/img' +str(count) + str(img_num) + '.png')
+        save_image( img,'D:/work/projects/3D-Crater/Dataset/augmented_images/img' +str(count) + str(img_num) + '.png')
         count +=1
     img_num += 1
---
+++ test.py
@@ -0,0 +1,7 @@
+import numpy as np
+from numpy import reshape
+from numpy import array
+
+data = [1 , 2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10]
+print(data.shape)
+#data = data.reshape(data.shape[0], -1)
